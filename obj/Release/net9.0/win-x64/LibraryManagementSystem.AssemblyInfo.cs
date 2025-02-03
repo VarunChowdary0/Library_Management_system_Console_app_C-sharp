cc62969df8785ae4acd3deb1d83836b0b378d624
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2025-02-03 22:07:43 +0530

    Build

--- obj/Release/net9.0/win-x64/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Release/net9.0/win-x64/LibraryManagementSystem.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e12707677021e38bd5e0fdbc1461b18dd54949")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
